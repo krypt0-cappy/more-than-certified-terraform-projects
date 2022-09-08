825abb7473a7c0376336b7bc74ff63288d3ae020
= <=>
2022-09-08 03:41:53 +0000

    updates committed

--- main.tf
+++ main.tf
@@ -13,12 +13,36 @@ resource "docker_image" "nodered_image" {
   name = "nodered/node-red:latest"
 }
 
+
+resource "random_string" "random" {
+  length  = 4
+  special = false
+  upper   = false
+}
+
+resource "random_string" "random2" {
+  length  = 4
+  special = false
+  upper   = false
+}
+
+
 resource "docker_container" "nodered_container" {
-  name  = "nodered"
+  name  = join("-", ["nodered", random_string.random.result])
   image = docker_image.nodered_image.latest
   ports {
     internal = 1880
     external = 1880
+  }
+}
+
+
+resource "docker_container" "nodered_container2" {
+  name  = join("-", ["nodered", random_string.random2.result])
+  image = docker_image.nodered_image.latest
+  ports {
+    internal = 1880
+    #external = 1880
   }
 }
 
@@ -29,5 +53,15 @@ output "IP-Address" {
 
 output "Container-name" {
   value = docker_container.nodered_container.name
+  description = "The name of your container"
+}
+
+output "IP-Address2" {
+  value = join(":", [docker_container.nodered_container2.ip_address, docker_container.nodered_container2.ports[0].external])
+  description = "The IP address and external port of your container"
+}
+
+output "Container-name2" {
+  value = docker_container.nodered_container2.name
   description = "The name of your container"
 }
